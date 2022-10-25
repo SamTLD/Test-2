cd2ad8136e4cb83dba5eaf1f696da2acea6693f8
SamTLD <111151431+SamTLD@users.noreply.github.com>
2022-10-25 16:56:32 +0530

    edit4

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-let firstName = 'Sam';
+/*let firstName = 'Sam';
 let lastName = 'Arun';
 console.log(firstName,lastName);
 
@@ -6,9 +6,23 @@ let interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
 
-/*let firstName = 'Sam';
-let lastName = 'Arun';*/
+let firstName = 'Sam';
+let lastName = 'Arun';
 let age = 26;
 let color = undefined;
 let adderss = null;
-console.log(firstName, lastName, age, adderss, color);
+console.log(firstName, lastName, age, adderss, color);*/
+
+let person = {
+    name:'sam',
+    age:30
+};
+//Dot Notation
+person.age = 20;
+
+//Bracket Notation
+let selection = 'name';
+
+person[selection] = 'Sami';
+
+console.log(person.age,person.name);
