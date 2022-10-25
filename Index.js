ef7b22cb822a992f0edce08e3e3cfbace74c0518
SamTLD <111151431+SamTLD@users.noreply.github.com>
2022-10-25 16:08:58 +0530

    edit4

--- Index.js
+++ Index.js
@@ -5,3 +5,10 @@ console.log(firstName,lastName);
 let interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
+
+/*let firstName = 'Sam';
+let lastName = 'Arun';*/
+let age = 26;
+let color = undefined;
+let adderss = null;
+console.log(firstName, lastName, age, adderss, color);
