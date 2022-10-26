21a62977ae420863f0c2683a75668a6ab03298d5
SamTLD <111151431+SamTLD@users.noreply.github.com>
2022-10-26 16:47:58 +0530

    edit6

--- Index.js
+++ Index.js
@@ -11,7 +11,7 @@ let lastName = 'Arun';
 let age = 26;
 let color = undefined;
 let adderss = null;
-console.log(firstName, lastName, age, adderss, color);*/
+console.log(firstName, lastName, age, adderss, color);
 
 let person = {
     name:'sam',
@@ -26,3 +26,59 @@ let selection = 'name';
 person[selection] = 'Sami';
 
 console.log(person.age,person.name);
+
+let selectedColors = ['red','blue','white'];
+selectedColors[3] = 'black';
+selectedColors[4] = 5;
+console.log(selectedColors);
+console.log(selectedColors.length);
+
+//Perfoming a task
+function one(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+one('sam', 'Arun');
+
+//Calculating a value
+function square(){
+
+}
+
+console.log('Hello world');
+
+let firstName = 'sam';
+let anousment = 'Hello! ';
+let middleAnousment = 'Your age is ';
+console.log(anousment ,firstName);
+
+let age = 20;
+age = 30;
+console.log(anousment,firstName,middleAnousment,age);
+
+let person = {
+    age : 20,
+    firstName : 'sam',
+}
+person.age = 30;
+let anousments = {
+    firstAnousment : 'Hello!',
+    secondAnousment : 'Your age is',
+}
+let selection = 'firstAnousment';
+anousments[selection] = 'Hi!';
+anousments.firstAnousment = 'Yo...';
+
+console.log(anousments.firstAnousment,person.firstName,anousments.secondAnousment,person.age);
+
+let country = ['sriLanka','india','america','russia'];
+country[4] = 'canada';
+
+console.log(country);
+
+let person = {
+    age : 30, 
+    name : 'Sam'
+}
+person.name = 'Sami';
+console.log(person.name,person.age);*/
+
